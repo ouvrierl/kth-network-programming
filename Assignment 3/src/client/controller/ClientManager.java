63088f5d9d22bf57cfce81a5df8abaec43c44f2e
ouvrierl <lucie.ouvrier-buffet@grenoble-inp.org>
2017-12-01 10:03:17 +0100

    Update ClientManager.java

--- Assignment 3/src/client/controller/ClientManager.java
+++ Assignment 3/src/client/controller/ClientManager.java
@@ -22,11 +22,10 @@ public class ClientManager {
 	private Controller controller;
 
 	public ClientManager(Stage stage) throws MalformedURLException, RemoteException, NotBoundException {
+		// Connections created at the beginning (RMI + Socket)
 		this.server = (CatalogServer) Naming.lookup("rmi://localhost/" + CatalogServer.SERVER_NAME_IN_REGISTRY);
-		this.serverReader = new ServerReader(); // Instance created for each new
-												// client
-		this.server.incomingClient(this.serverReader); // Inform the server of
-														// the new arrival
+		this.serverReader = new ServerReader(); // Instance created for each new client
+		this.server.incomingClient(this.serverReader); // Inform the server of the new arrival
 		this.controller = new Controller();
 		this.controller.connect(ADDRESS, PORT); // Socket launched
 		this.stage = stage;

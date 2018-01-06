59482de3061f243560ad22ba104af5e7bf460554
ouvrierl <lucie.ouvrier-buffet@grenoble-inp.org>
2018-01-06 15:35:02 +0100

    project begin

---
+++ Project/WhiteboardApp/src/main/java/kth/ads/Database.java
@@ -0,0 +1,182 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kth.ads;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lucieouvrier-buffet
+ */
+public class Database {
+    
+    private static final String TABLE_NAME_USER = "aduser";
+    private static final String TABLE_NAME_AD = "advertisement";
+    private PreparedStatement createAccount;
+    private PreparedStatement createAd;
+    private PreparedStatement checkAccount;
+    private PreparedStatement checkEmail;
+    private PreparedStatement getLastId;
+    private PreparedStatement getListAds;
+    private PreparedStatement getAd;
+    
+    public Database() {
+        try {
+            Connection connection = this.connection();
+            this.prepareStatements(connection);
+        } catch (ClassNotFoundException | SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+    
+    private Connection connection() throws ClassNotFoundException, SQLException {
+        try{
+            String dbURL = "jdbc:derby://localhost:1527/sample;create=false;user=app;password=app";
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            return DriverManager.getConnection(dbURL);    
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private void prepareStatements(Connection connection) throws SQLException {
+        this.createAccount = connection.prepareStatement("INSERT INTO " + TABLE_NAME_USER + " VALUES (?, ?)");
+        this.createAd = connection.prepareStatement("INSERT INTO " + TABLE_NAME_AD + " (title, description, price, category, useremail, id, location) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        this.checkAccount = connection.prepareStatement("SELECT * from " + TABLE_NAME_USER + " WHERE email = ? AND password = ?");
+        this.checkEmail = connection.prepareStatement("SELECT * from " + TABLE_NAME_USER + " WHERE email = ?");
+        this.getLastId = connection.prepareStatement("SELECT MAX(ID) FROM " + TABLE_NAME_AD);
+        this.getListAds = connection.prepareStatement("SELECT * FROM " + TABLE_NAME_AD);
+        this.getAd = connection.prepareStatement("SELECT * FROM " + TABLE_NAME_AD + " WHERE ID = ?");
+    }
+    
+    public boolean createAccount(String email, String password) {
+        try {
+            this.checkEmail.setString(1, email);
+            ResultSet result = this.checkEmail.executeQuery();
+            if (result.next()) {
+                // Username already used
+                return false;
+            }
+            this.createAccount.setString(1, email);
+            this.createAccount.setString(2, password);
+            int rows = this.createAccount.executeUpdate();
+            return rows == 1;
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean isAccount(String email, String password) {
+        ResultSet result = null;
+        try {
+            this.checkAccount.setString(1, email);
+            this.checkAccount.setString(2, password);
+            result = this.checkAccount.executeQuery();
+            if (result.next()) {
+                    // Ok, match
+                    return true;
+            }
+        } catch (SQLException sqle) {
+                sqle.printStackTrace();
+        } finally {
+            try {
+                    result.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean addAd(String title, String description, String price, String category, String user, String location) {
+        try {
+            int id = 0;
+            ResultSet result = this.getLastId.executeQuery();
+            if (result.next()) {
+                id = result.getInt(1) + 1;
+            }
+            else{
+                return false;
+            }
+            this.createAd.setString(1, title);
+            this.createAd.setString(2, description);
+            this.createAd.setString(3, price);
+            this.createAd.setString(4, category);
+            this.createAd.setString(5, user);
+            this.createAd.setString(6, Integer.toString(id));
+            this.createAd.setString(7, location);
+            int rows = this.createAd.executeUpdate();
+            return rows == 1;
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            return false;
+        }
+    }
+    
+    public List <String []> getAds(){
+        try{
+            ResultSet result = this.getListAds.executeQuery();
+            List <String []> ads = new ArrayList<>();
+            while (result.next()) {
+                String title = result.getString("title");
+                String description = result.getString("description");
+                String price = result.getString("price");
+                String category = result.getString("category");
+                String email = result.getString("useremail");
+                String location = result.getString("location");
+                String id = result.getString("id");
+                String [] ad = new String[7];
+                ad[0] = title;
+                ad[1] = description;
+                ad[2] = price;
+                ad[3] = category;
+                ad[4] = email;
+                ad[5] = location;
+                ad[6] = id;
+                ads.add(ad);
+            }
+            return ads;
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public String [] getAd(String id){
+        try{
+            this.getAd.setString(1, id);
+            ResultSet result = this.getAd.executeQuery();
+            result.next();
+            String title = result.getString("title");
+            String description = result.getString("description");
+            String price = result.getString("price");
+            String category = result.getString("category");
+            String email = result.getString("useremail");
+            String location = result.getString("location");
+            String [] ad = new String[7];
+            ad[0] = title;
+            ad[1] = description;
+            ad[2] = price;
+            ad[3] = category;
+            ad[4] = email;
+            ad[5] = location;
+            ad[6] = id;
+            return ad;
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
---
+++ Project/WhiteboardApp/src/main/webapp/websocket.js
@@ -0,0 +1,237 @@
+var wsUri = "ws://" + document.location.host + "/WhiteboardApp/" + "adsendpoint";
+var websocket = new WebSocket(wsUri);
+
+websocket.onerror = function(evt) { onError(evt) };
+
+websocket.onmessage = function(evt) { onMessage(evt) };
+
+function onError(evt) {
+}
+
+function sendText(json) {
+    while (websocket.readyState !== 1) {
+    }
+    console.log("sending text: " + json);
+    websocket.send(json);
+}
+
+function onMessage(message) {
+    var messageReceived = message.data;
+    console.log("received text: " + messageReceived);
+    var messageSplit = messageReceived.split(" ");
+    switch(messageSplit[0]){
+        case "login":
+            if(messageSplit[1] == "true"){
+                document.cookie = "email=" + messageSplit[2];
+                alert("You are now logged");
+            }
+            else{
+                alert("Impossible to login");
+            }
+            break;
+        case "create":
+            if(messageSplit[1] == "true"){
+                alert("The account has been created");
+            }
+            else{
+                alert("Impossible to create the account");
+            }
+            break;
+        case "ad":
+            if(messageSplit[1] == "true"){
+                alert("The advertisement has been added");
+            }
+            else{
+                alert("Impossible to add the advertisement");
+            }
+            break;
+        case "list":
+            var dataComplete = "";
+            for(i = 1; i < messageSplit.length; i++){
+                dataComplete += messageSplit[i];
+            }
+            var list = dataComplete;
+            var adData = list.split("[");
+            var nbAds = adData.length;
+            var toRemove = document.getElementById("table");
+            if(toRemove != null){
+                toRemove.outerHTML = "";
+                delete toRemove;    
+            }
+            var body = document.getElementsByTagName('body')[0];
+            var table = document.createElement('table');
+            table.setAttribute("id", "table");
+            var tbody = document.createElement('tbody');
+            for(var i = 0; i < nbAds; i++){
+                var tr = document.createElement('tr');
+                var row = adData[i].substring(0, adData[i].length - 1);
+                var rowSplit = row.split("{");
+                console.log(row);
+                var id;
+                for(var j = 0; j < rowSplit.length; j++){
+                    var valueData = rowSplit[j].substring(0, rowSplit[j].length - 1);
+                    console.log(valueData);
+                    if(j == rowSplit.length - 1){
+                        id = valueData;
+                    }
+                    var td = document.createElement('td');
+                    td.appendChild(document.createTextNode(valueData));
+                    tr.appendChild(td)
+                }
+                tr.setAttribute("onclick", "document.location = './ad.html?id=" + id + "';");
+                tbody.appendChild(tr);
+            }
+            table.appendChild(tbody);
+            body.appendChild(table)
+            break;
+        case "get":
+            var dataComplete = "";
+            for(i = 1; i < messageSplit.length; i++){
+                dataComplete += messageSplit[i];
+            }
+            var list = dataComplete;
+            var rowSplit = list.split("{");
+            var toRemove = document.getElementById("table");
+            if(toRemove != null){
+                toRemove.outerHTML = "";
+                delete toRemove;    
+            }
+            var body = document.getElementsByTagName('body')[0];
+            var table = document.createElement('table');
+            table.setAttribute("id", "table");
+            var tbody = document.createElement('tbody');
+            for(var j = 0; j < rowSplit.length; j++){
+                var tr = document.createElement('tr');
+                var valueData = rowSplit[j].substring(0, rowSplit[j].length - 1);
+                console.log(valueData);
+                if(j == rowSplit.length - 1){
+                    id = valueData;
+                }
+                var td = document.createElement('td');
+                if(j == 1){
+                    td.appendChild(document.createTextNode("Title: "));    
+                }
+                else if (j == 2){
+                    td.appendChild(document.createTextNode("Description: "));    
+                }
+                else if (j == 3){
+                    td.appendChild(document.createTextNode("Price: "));    
+                }
+                else if (j == 4){
+                    td.appendChild(document.createTextNode("Category: "));    
+                }
+                else if (j == 5){
+                    td.appendChild(document.createTextNode("Seller: "));    
+                }
+                else if (j == 6){
+                    td.appendChild(document.createTextNode("Location: "));    
+                }
+                else if (j == 7){
+                    td.appendChild(document.createTextNode("Advertisement number: "));    
+                }
+                tr.appendChild(td);
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(valueData));
+                tr.appendChild(td);
+                tbody.appendChild(tr);
+            }
+            table.appendChild(tbody);
+            body.appendChild(table)
+            break;
+    }
+}
+
+function addAd(event){
+    if(getUser() == ""){
+        alert("You must be logged to add an advertisement");
+    }
+    else{
+        title = document.getElementById("title").value;
+        description = document.getElementById("description").value;
+        price = document.getElementById("price").value;
+        picture = document.getElementById("picture").value;
+        category = document.getElementById("category").value;
+        adLocation = document.getElementById("adLocation").value;
+        user = getUser();
+        var json = JSON.stringify({
+            "type": "ad",
+            "title": title,
+            "description": description,
+            "price": price,
+            "category": category,
+            "user": user,
+            "adLocation": adLocation
+        });
+        sendText(json);
+    }
+    return false;
+}
+
+function getUser(){
+    return getCookie("email");
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function login(email, password){
+    var json = JSON.stringify({
+        "type": "login",
+        "email": email,
+        "password": password
+    });
+    sendText(json);
+    return false;
+}
+                
+function create(email, password){
+    var json = JSON.stringify({
+        "type": "create",
+        "email": email,
+        "password": password
+    });
+    sendText(json);
+    return false;
+}
+
+function logout(event){
+    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    alert("You are now unlogged");
+    return false;
+}
+
+function getList(){
+    var json = JSON.stringify({
+        "type": "list"
+    });
+    sendText(json);
+    return false;
+}
+
+function getAd(){
+    var json = JSON.stringify({
+        "type": "get",
+        "id": window.location.search.substr(4)
+    });
+    sendText(json);
+    return false;
+}
+
+function getAdNumber(){
+    var hNumber = document.getElementById("adNumber");
+    var id = window.location.search.substr(4);
+    hNumber.innerHTML = "Advertisement " + id;
+}
